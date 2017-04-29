b30ec46930c4d32bb7a58608d1d11bd1e3bb379f
Gabriel Brammer <brammer@stsci.edu>
2017-04-29 15:18:19 -0400

    Add polynomial evaluation functions that are easier to read and separate out the field dependence

--- grismconf/grismconf.py
+++ grismconf/grismconf.py
@@ -357,3 +357,162 @@ class Config(object):
         sens[-2:] = 0.
                 
         return [wavs,sens]
+
+def testing():
+    
+    # NIRISS
+    gr, filter = 'C', 'F150W'
+    order = 'A'
+    conf = grismconf.grismconf.Config('GR150{0}.{1}.t.conf'.format(gr, filter))
+    
+    t = np.linspace(0,1,10)
+    x0, y0 = 1536, 1536
+
+    # dx, forward polynomial
+    dx = conf.DISPX(order, x0, y0, t)
+    dxp = forward(conf._DISPX_data[order], x0, y0, t)
+    np.testing.assert_array_almost_equal(dx, dxp, decimal=4)
+    
+    # inverse
+    ti = conf.INVDISPX(order, x0, y0, dx)
+    tip = inverse(conf._DISPX_data[order], x0, y0, dx)
+    
+    np.testing.assert_array_almost_equal(t, ti, decimal=4)
+    np.testing.assert_array_almost_equal(ti, tip, decimal=4)
+    
+    # Derivative is different, correct for `deriv`
+    ddx = conf.DDISPX(order, x0, y0, t) 
+    ddxp = deriv(conf._DISPX_data[order], x0, y0, t) 
+    
+    print('DDISPX:',ddx)  # single float
+    print('deriv:', ddxp) # evaluated at all t 
+    
+    
+def forward(coeffs, x0, y0, t):
+    """Forward evaluation of field-dependent polynomials f(x0,y0,t)
+    
+    Parameters
+    ----------
+    x0, y0 : float 
+        Coordinate to evaluate the field dependent coefficients
+    
+    t : float or array-like
+        Independent variable at which to evaluate the polynomial
+    
+    Returns
+    -------
+    d : float or array-like
+        Evaluated value at f(x0,y0,t).
+    
+    """
+    a_i = field_dependent(x0, y0, coeffs)
+    d = np.polyval(a_i[::-1], t)
+    return d
+
+def deriv(coeffs, x0, y0, t):
+    """df/dt of the field-dependent polynomials f(x0,y0,t)
+    
+    Parameters
+    ----------
+    x0, y0 : float 
+        Coordinate to evaluate the field dependent coefficients
+    
+    t : float or array-like
+        Independent variable at which to evaluate the polynomial
+    
+    Returns
+    -------
+    d : float or array-like
+        Evaluated derivative at df(x0,y0,t)/dt.
+    
+    """
+    a_i = field_dependent(x0, y0, coeffs)
+    if len(a_i) == 1:
+        return t*0.
+    
+    power = np.arange(len(a_i))
+    a_i_prime = (a_i*power)[1::][::-1]
+    d = np.polyval(a_i_prime, t)
+    return d
+    
+def inverse(coeffs, x0, y0, d):
+    """Inverse of field-dependent polynomials, d = f(x0,y0,t)
+    
+    Parameters
+    ----------
+    x0, y0 : float 
+        Coordinate to evaluate the field dependent coefficients
+    
+    d : float or array-like
+        Dependent variable at which to compute the roots, `t`, i.e., 
+        `d = f(x0,y0,t)`.
+    
+    Returns
+    -------
+    t : float or array-like
+        Independent variable.
+        
+    .. note::
+        
+        Current implementation is analytic for polynomial orders 0 and 1.  
+        Uses `~numpy.poly1d` to compute roots of higher order polynomials and
+        currently returns all roots, i.e., the two roots for a quadratic
+        function.
+    
+    """
+    from scipy import polyval
+    a_i = field_dependent(x0, y0, coeffs)
+    if len(a_i) == 1:
+        t = d*0.+a_i
+    elif len(a_i) == 2:
+        t = (d-a_i[0])/a_i[1]
+    else:
+        # Check for higher order functions
+        p = np.poly1d(a_i[::-1])
+        t = np.squeeze([(p-di).roots[0] for di in d])
+
+    return t
+    
+def field_dependent(x0, y0, coeffs):
+    """aXe-like field-dependent coefficients
+    
+    See the `aXe manual <http://axe.stsci.edu/axe/manual/html/node7.html#SECTION00721200000000000000>`_ for a description of how the field-dependent coefficients are specified.
+    
+    Parameters
+    ----------
+    x0, y0 : float or array-like
+        Coordinate to evaluate the field dependent coefficients, where
+    
+    coeffs : array-like
+        Field-dependency coefficients
+    
+    Returns
+    -------
+    a : float or array-like
+        Evaluated field-dependent coefficients
+    
+    .. note::
+    
+    Taken from `~grizli`: 
+    https://github.com/gbrammer/grizli/blob/master/grizli/grismconf.py#L127.
+    
+    """
+    ## number of coefficients for a given polynomial order
+    ## 1:1, 2:3, 3:6, 4:10, order:order*(order+1)/2
+    if isinstance(coeffs, float):
+        order = 1
+    else:
+        order = int(-1+np.sqrt(1+8*coeffs.shape[-1])) // 2
+
+    ## Build polynomial terms array
+    ## $a = a_0+a_1x_i+a_2y_i+a_3x_i^2+a_4x_iy_i+a_5yi^2+$ ...
+    xy = []
+    for p in range(order):
+        for px in range(p+1):
+            #print 'x**%d y**%d' %(p-px, px)
+            xy.append(x0**(p-px)*y0**(px))
+
+    ## Evaluate the polynomial, allowing for N-dimensional inputs
+    a = np.sum((np.array(xy).T*coeffs).T, axis=0)
+
+    return a
