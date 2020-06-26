2632f4c78a832e097928b86a205b7120f1058e2c
Nor Pirzkal <npirzkal@stsci.edu>
2020-06-25 20:32:16 -0400

    added url

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ def read(fname):
 
 setup(
 	name='grismconf',
-	version='1.0',
+	version='1.21',
 	description='GRISM Configuration python code, described in ISR 2017-01: A More Generalized Coordinate Transformation Approach for Grisms, Pirzkal & Ryan 2017 http://www.stsci.edu/hst/wfc3/documents/ISRs/WFC3-2017-01.pdf',
+	url='https://github.com/npirzkal/GRISMCONF',
 	author='Nor Pirzkal',
 	author_email='npirzkal@mac.com',
     package_dir = {
